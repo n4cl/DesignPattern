1063d8c23d745e2e88d5bc6aa49b918680767828
n4cl <devn4cl@gmail.com>
2023-10-30 00:09:02 +0900

    Update: Python 3 系に対応するように更新

--- Adapter/Sample1/main.py
+++ Adapter/Sample1/main.py
@@ -1,6 +1,46 @@
-# coding: utf-8
+from abc import abstractmethod, ABCMeta
 
-from PrintBanner import PrintBanner
+class Banner:
+    """
+    最初から提供されているとするクラス
+    必要な機能を追加するのではなく、
+    当クラスを継承して、必要な機能仕様を満たすようにする
+    """
+
+    def __init__(self, string):
+        self.string = string
+
+    def show_with_paren(self):
+        print(f"({self.string})")
+
+    def show_with_aster(self):
+        print(f"*{self.string}*")
+
+class Print(metaclass=ABCMeta):
+    """
+    interface の役割
+    必要とされる仕様
+    """
+
+    @abstractmethod
+    def print_weak(self):
+        pass
+
+    @abstractmethod
+    def print_strong(self):
+        pass
+
+class PrintBanner(Banner, Print):
+    """
+    Adapterとなるクラス
+    Bannerクラスを継承し、Printクラスのinterfaceを実装する
+    """
+
+    def print_weak(self):
+        self.show_with_paren()
+
+    def print_strong(self):
+        self.show_with_aster()
 
 
 def main():
