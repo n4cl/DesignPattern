7b028ef00227ebeb6a722131a0b2f804b16ae578
n4cl <devn4cl@gmail.com>
2023-11-07 00:18:23 +0900

    Update: Singleton パターンにてマルチスレッドでも対応できるように更新

--- Singleton/main.py
+++ Singleton/main.py
@@ -1,5 +1,8 @@
+import threading
+
 class Singleton:
     __singleton = None
+    __lock = threading.Lock()
 
     def __init__(self) -> None:
         print("インスタンスを生成しました。")
@@ -9,7 +12,7 @@ class Singleton:
         cls.__singleton = Singleton()
 
     @classmethod
-    def getInstance(cls):
+    def get_instance(cls):
         """
         マルチスレッド下での動作を考慮していない
         """
@@ -17,15 +20,29 @@ class Singleton:
             cls.__initialize()
         return cls.__singleton
 
+    @classmethod
+    def get_instance_thread_safe(cls):
+        """
+        マルチスレッド下での動作を考慮した場合
+        """
+        # ロックを取得するオーバーヘッドを避けるため
+        if cls.__singleton is None:
+            with cls.__lock:
+                # ロック解放後にすでにインスタンスが生成されている可能性があるため
+                if cls.__singleton is None:
+                    cls.__initialize()
+        return cls.__singleton
+
 
 def main():
     print("Start.")
-    obj1 = Singleton.getInstance()
-    obj2 = Singleton.getInstance()
-    if obj1 == obj2:
-        print("obj1とobj2は同じインスタンスです。")
+    obj1 = Singleton.get_instance()
+    obj2 = Singleton.get_instance()
+    obj3 = Singleton.get_instance_thread_safe()
+    if obj1 == obj2 == obj3:
+        print("obj1とobj2とobj3は同じインスタンスです。")
     else:
-        print("obj1とobj2は同じインスタンスではありません。")
+        print("obj1とobj2とobj3は同じインスタンスではありません。")
     print("End.")
 
 
